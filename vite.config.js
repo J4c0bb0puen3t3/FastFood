bca4b7c1d57a3a81a04f9a27f76ad2e8d623d960
JAcobo <jacobocarlos72@gmail.com>
2026-02-10 11:32:09 -0600

    chande m & i

--- vite.config.js
+++ vite.config.js
@@ -23,12 +23,12 @@ export default defineConfig({
         icons: [
           {
             src: "icons/icon-192.png",
-            sizes: "50x50",
+            sizes: "192x192",
             type: "image/png",
           },
           {
             src: "icons/icon-512.png",
-            sizes: "50x50",
+            sizes: "512x512",
             type: "image/png",
           }
         ],
