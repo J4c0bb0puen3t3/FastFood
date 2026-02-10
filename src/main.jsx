7c8bf7d0a231fe1f3af7c1b64612a2af16838a4c
JAcobo <jacobocarlos72@gmail.com>
2026-02-10 09:07:07 -0600

    change router

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './app/App'
+import { HashRouter } from "react-router-dom";
 import { registerSW } from "virtual:pwa-register"; // 👉 registro del Service Worker (PWA)
 
 registerSW({
@@ -14,7 +15,7 @@ registerSW({
 });
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <HashRouter>
+  <App />
+</HashRouter>
 )
